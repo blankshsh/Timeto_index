15350ce1a5298c01e0661731b06ada0961ba954c
blankshsh <blankshsh@qq.com>
2016-10-31 22:14:27 +0800

    更新动画delay

--- build/js/function.js
+++ build/js/function.js
@@ -1,30 +1,30 @@
-function bgchange() {
+function BgSize() {
+    // 自适应背景图片
     var width = $(window).width();
     //console.log(width);
     $("header").width(width);
 }
-bgchange();
-
-
 function animation(id, cfg) {
-    var scrolled = $(window).scrollTop(),
-        win_height_padded = $(window).height() * 1.1;
+    // 获取id图片 判断距顶部位置
+    // 如果滚轮滚到图片位置
+    // 就运行animation
+    var $window_ = $(window),
+        scrolled_ = $window_.scrollTop(),
+        height_ = $window_.height() * 1.1;
     $(id).each(function() {
         var $this = $(this),
             offsetTop = $this.offset().top;
-        if (scrolled + win_height_padded > offsetTop) {
+        if (scrolled_ + height_ > offsetTop) {
             setTimeout(function() {
-                cfg.animateIn && $this.animate(cfg.animateIn);
+                cfg.animateIn && $this.animate(cfg.animateIn,1000);
             }, cfg.delay || 0)
         }
     })
 }
-window.onresize = function() {
-    bgchange();
-}
-$(window).scroll(function() {
-    addanimation();
-});
+BgSize();
+$(window).resize(function(){
+    BgSize();
+})
 console.log("by:blankshsh");
 console.log("github.com/blankshsh");
 console.log("blankshsh@163.com");
--- build/js/index.js
+++ build/js/index.js
@@ -1,36 +1,49 @@
 function addanimation() {
-    animation(animation_page1, {
+    animation("#animation_page1", {
         animateIn: {
             opacity: 1,
             right: 50,
-        },
-        animateOut: {
-            opacity: 0,
-            right: -50,
-        },
+        }
     })
-    animation(animation_page2, {
+    animation("#animation_page2", {
         animateIn: {
             opacity: 1,
             left: 40,
         },
-        animateOut: {
-            opacity: 1,
-            left: -40,
-        },
+        delay: 200,
     })
-    animation(animation_page3, {
+    animation("#animation_page3", {
         animateIn: {
             opacity: 1,
             right: 30,
         },
-        animateOut: {
-            opacity: 0,
-            right: -50,
-        },
+        delay: 200,
     })
+    animation(".page1wrp .info", {
+        animateIn: {
+            opacity: 1,
+            top: 260,
+        },
+        delay: 1000,
+    })
+    animation(".page2wrp .info", {
+        animateIn: {
+            opacity: 1,
+            top: 254,
+        },
+        delay: 1000,
+    })
+    animation(".page3wrp .info", {
+        animateIn: {
+            opacity: 1,
+            left: 24,
+        },
+        delay: 1000,
+    })
+
 }
 
-$(window).scroll(function () {
-	addanimation();
+addanimation();
+$(window).scroll(function() {
+    addanimation();
 });
